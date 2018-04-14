3660be9ed9ebde0eeac6334139f68ad9e733f1f4
Andrew Euredjian <andrew_103@rocketmail.com>
2018-04-14 16:07:00 -0400

    Init branch for getting motor control down using combination of gyro and encoders

---
+++ fan_control/fan_control.ino
@@ -0,0 +1,12 @@
+#define FAN_MOTOR_A 3
+#define FAN_MOTOR_B 5
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
