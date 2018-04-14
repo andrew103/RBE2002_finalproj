5304d51553aead4093bc11d10b578369275b82be
Andrew Euredjian <andrew_103@rocketmail.com>
2018-04-14 16:28:14 -0400

    Write basic code for running the fan using pololu motor controller

--- fan_control/fan_control.ino
+++ fan_control/fan_control.ino
@@ -3,10 +3,34 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(FAN_MOTOR_A, OUTPUT);
+  pinMode(FAN_MOTOR_B, OUTPUT);
 
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  run_fan(255);
+  // delay(10000);
 
+  // run_fan(0);
+  // delay(15000);
+}
+
+void run_fan(int speed) {
+  speed = constrain(speed, -255, 255);
+
+  if (speed > 0) {
+    analogWrite(FAN_MOTOR_A, speed);
+    analogWrite(FAN_MOTOR_B, 0);
+  }
+  else if (speed < 0) {
+    analogWrite(FAN_MOTOR_A, 0);
+    analogWrite(FAN_MOTOR_B, speed);
+  }
+  else {
+    analogWrite(FAN_MOTOR_A, 0);
+    analogWrite(FAN_MOTOR_B, 0);
+  }
 }
