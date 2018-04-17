78474f18e5042a98df0bedf36f222d75c2a8d7aa
Andrew Euredjian <andrew_103@rocketmail.com>
2018-04-17 17:34:39 -0400

    Reformat with indentation

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -11,102 +11,96 @@ long duration;
 int distance;
 
 enum drivingStates {
- drive
+  drive
 };
 
 enum movingStates {
- forward,left,right
+  forward, left, right
 };
 
 drivingStates actions = drive;
 movingStates movingActions = forward;
 
 
-
-
-
-
 //sets up all the pins and library functions
 void setup() {
-pinMode(fronttrigPin, OUTPUT); // Sets the fronttrigPin as an Output
-pinMode(frontechoPin, INPUT); // Sets the frontechoPin as an Input
-pinMode(lefttrigPin, OUTPUT); // Sets the lefttrigPin as an Output
-pinMode(leftechoPin, INPUT); // Sets the leftechoPin as an Input
-pinMode(righttrigPin, OUTPUT); // Sets the righttrigPin as an Output
-pinMode(rightechoPin, INPUT); // Sets the rightechoPin as an Input
-Serial.begin(115200); // Starts the serial communication
+  pinMode(fronttrigPin, OUTPUT); // Sets the fronttrigPin as an Output
+  pinMode(frontechoPin, INPUT); // Sets the frontechoPin as an Input
+  pinMode(lefttrigPin, OUTPUT); // Sets the lefttrigPin as an Output
+  pinMode(leftechoPin, INPUT); // Sets the leftechoPin as an Input
+  pinMode(righttrigPin, OUTPUT); // Sets the righttrigPin as an Output
+  pinMode(rightechoPin, INPUT); // Sets the rightechoPin as an Input
+  Serial.begin(115200); // Starts the serial communication
 }
 
 int frontDistanceToWall() {
-// Clears the trigPin
-digitalWrite(fronttrigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(fronttrigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(fronttrigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(frontechoPin, HIGH);
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-return distance;
+  // Clears the trigPin
+  digitalWrite(fronttrigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(fronttrigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(fronttrigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(frontechoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  return distance;
 }
 
 int leftDistanceToWall() {
-// Clears the trigPin
-digitalWrite(lefttrigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(lefttrigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(lefttrigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(leftechoPin, HIGH);
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-return distance;
+  // Clears the trigPin
+  digitalWrite(lefttrigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(lefttrigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(lefttrigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(leftechoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  return distance;
 }
 
 int rightDistanceToWall() {
-// Clears the trigPin
-digitalWrite(righttrigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(righttrigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(righttrigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(rightechoPin, HIGH);
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-return distance;
+  // Clears the trigPin
+  digitalWrite(righttrigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(righttrigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(righttrigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(rightechoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  return distance;
 }
 
 void loop() {
-  switch(actions){
+  switch (actions) {
     case drive:
-                switch(movingActions){
-                  case forward: if(frontDistanceToWall() >= 14){
-                                  /*
-                                   * put code for moving forward
-                                   */
-                                }
-                  case left:  if(rightDistanceToWall() <= 14){
-                                /*
-                                 * put code to turn left
-                                 */
-                              }
-                  case right:  if(rightDistanceToWall() > 14){
-                                /*
-                                 * put code to turn right
-                                 */
-                              }                  
-                }
-
-                
+      switch (movingActions) {
+        case forward: if (frontDistanceToWall() >= 14) {
+            /*
+               put code for moving forward
+            */
+          }
+        case left:  if (rightDistanceToWall() <= 14) {
+            /*
+               put code to turn left
+            */
+          }
+        case right:  if (rightDistanceToWall() > 14) {
+            /*
+               put code to turn right
+            */
+          }
+      }
   }
 }
 
