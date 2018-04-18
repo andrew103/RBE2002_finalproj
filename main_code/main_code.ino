d55825cc61b7fc4f41562992bd5975b5b78cfea1
mahajanrevant <revantmahajan1999@gmail.com>
2018-04-18 16:50:53 -0400

    Wall following code to be tested

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,3 +1,15 @@
+#include <PID_v1.h>
+
+#define L_MOTOR_A 32
+#define L_MOTOR_B 33
+#define R_MOTOR_A 25
+#define R_MOTOR_B 26
+
+#define LA_CHANNEL 2
+#define LB_CHANNEL 3
+#define RA_CHANNEL 4
+#define RB_CHANNEL 5
+
 // defines pins numbers
 const int fronttrigPin = 2;
 const int frontechoPin = 4;
@@ -8,19 +20,23 @@ const int rightechoPin = 22;
 
 // defines variables
 long duration;
-int distance;
+double distance;
+double setpoint, input, output;
+double Kp=2, Ki=5, Kd=1;
+
 
 enum drivingStates {
   drive
 };
 
 enum movingStates {
-  forward, left, right
+  forward,turnLeft
 };
 
 drivingStates actions = drive;
 movingStates movingActions = forward;
 
+PID myPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
 
 //sets up all the pins and library functions
 void setup() {
@@ -31,9 +47,47 @@ void setup() {
   pinMode(righttrigPin, OUTPUT); // Sets the righttrigPin as an Output
   pinMode(rightechoPin, INPUT); // Sets the rightechoPin as an Input
   Serial.begin(115200); // Starts the serial communication
+   
+  setpoint = 14.5;
+  myPID.SetMode(AUTOMATIC);
+
+    ledcSetup(LA_CHANNEL, 100, 8);
+  ledcAttachPin(L_MOTOR_A, LA_CHANNEL);
+
+  ledcSetup(LB_CHANNEL, 100, 8);
+  ledcAttachPin(L_MOTOR_B, LB_CHANNEL);
+
+  ledcSetup(RA_CHANNEL, 100, 8);
+  ledcAttachPin(R_MOTOR_A, RA_CHANNEL);
+
+  ledcSetup(RB_CHANNEL, 100, 8);
+  ledcAttachPin(R_MOTOR_B, RB_CHANNEL);
 }
 
-int frontDistanceToWall() {
+void drive_motor(int lmotor, int rmotor) {
+  lmotor = constrain(lmotor, -255, 255);
+  rmotor = constrain(rmotor, -255, 255);
+
+  if (lmotor > 0) {
+    ledcWrite(LA_CHANNEL, lmotor);
+    ledcWrite(LB_CHANNEL, 0);
+  }
+  else {
+    ledcWrite(LA_CHANNEL, 0);
+    ledcWrite(LB_CHANNEL, -lmotor);
+  }
+
+  if (rmotor > 0) {
+    ledcWrite(RA_CHANNEL, rmotor);
+    ledcWrite(RB_CHANNEL, 0);
+  }
+  else {
+    ledcWrite(RA_CHANNEL, 0);
+    ledcWrite(RB_CHANNEL, -rmotor);
+  }
+}
+
+double frontDistanceToWall() {
   // Clears the trigPin
   digitalWrite(fronttrigPin, LOW);
   delayMicroseconds(2);
@@ -49,7 +103,7 @@ int frontDistanceToWall() {
   return distance;
 }
 
-int leftDistanceToWall() {
+double leftDistanceToWall() {
   // Clears the trigPin
   digitalWrite(lefttrigPin, LOW);
   delayMicroseconds(2);
@@ -65,7 +119,7 @@ int leftDistanceToWall() {
   return distance;
 }
 
-int rightDistanceToWall() {
+double rightDistanceToWall() {
   // Clears the trigPin
   digitalWrite(righttrigPin, LOW);
   delayMicroseconds(2);
@@ -85,21 +139,27 @@ void loop() {
   switch (actions) {
     case drive:
       switch (movingActions) {
-        case forward: if (frontDistanceToWall() >= 14) {
-            /*
-               put code for moving forward
-            */
-          }
-        case left:  if (rightDistanceToWall() <= 14) {
-            /*
-               put code to turn left
-            */
-          }
-        case right:  if (rightDistanceToWall() > 14) {
-            /*
-               put code to turn right
-            */
-          }
+        case forward: if (frontDistanceToWall() >= 8) {
+         
+                      input = rightDistanceToWall();
+                      myPID.Compute();
+          
+                       if (rightDistanceToWall() < 14) {
+                           drive_motor(100, 120 + output);
+                        }
+                      else if (rightDistanceToWall() > 15) {
+                           drive_motor(120 + output, 100);
+                        }
+                 
+                      else{
+                          drive_motor(120, 120);
+                      }
+                     }
+                     else {
+                      movingActions = turnLeft;
+                     }
+                     break;
+         case turnLeft: break;
       }
   }
 }
