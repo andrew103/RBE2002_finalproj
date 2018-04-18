888e49628d747f89dccb34e5f1054c45021b1f15
Andrew Euredjian <andrew_103@rocketmail.com>
2018-04-18 01:10:45 -0400

    Change pin values for drivebase motors

--- drivebase_control/drivebase_control.ino
+++ drivebase_control/drivebase_control.ino
@@ -1,12 +1,14 @@
 #define L_MOTOR_A 32
 #define L_MOTOR_B 33
-#define R_MOTOR_A 34
-#define R_MOTOR_B 35
+#define R_MOTOR_A 25
+#define R_MOTOR_B 26
 
 #define LA_CHANNEL 2
 #define LB_CHANNEL 3
 #define RA_CHANNEL 4
 #define RB_CHANNEL 5
+
+#define ENC_CPR 48
 
 void setup() {
   ledcSetup(LA_CHANNEL, 100, 8);
