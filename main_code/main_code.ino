29a88540ebd0dbf0c058789a9c7758fd6c28e660
arjungandhi <arjungandhi06@gmail.com>
2018-05-02 01:13:48 -0400

    Fix a stupid

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -538,7 +538,7 @@ void loop() {
       }
 
       if (rHome){
-        if(global_xpos<250, global_ypos<250){
+        if(global_xpos<250 || global_ypos<250){
           lcd.clear();
           lcd.setCursor(0,0);
           lcd.print("lol made it bub");
@@ -753,6 +753,5 @@ void renc_isr() {
 }
 
 void cliff_isr() {
-  delay(10);
   movingActions = reverse;
 }
